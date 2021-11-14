7c9f0c3d1b3ab017d55cf33c8a7a651ec9ef61ad
Nicolas <nicolasadrianfranco@gmail.com>
2021-11-14 16:16:17 -0300

    actualización de  appId en firebase

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.appId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
