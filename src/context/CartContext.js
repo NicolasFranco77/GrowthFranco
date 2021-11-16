6feba1630c0f377aabfbd0424afb3fd3bee74c27
Nicolas <nicolasadrianfranco@gmail.com>
2021-11-16 10:18:18 -0300

    fix localStorage

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,48 +3,50 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 export const CartContextProvider = ({ children }) => {
-  const cartStorage = JSON.parse(window.localStorage.getItem("text"));
-  const [productsCart, setProductsCart] = useState(cartStorage);
+  const cartStorage = JSON.parse(window.localStorage.getItem("cartItems"));
+  const [productsCart, setProductsCart] = useState(cartStorage || [] );
   const [totalPrice, setTotalPrice] = useState(0);
 
   const setLocalStorage = (value) => {
     try {
       setProductsCart(value);
-      window.localStorage.setItem("text", JSON.stringify(value));
+      window.localStorage.setItem("cartItems", JSON.stringify(value));
     } catch (error) {
       console.log(error);
     }
   };
 
+
+
   const getQuantity = () => {
     let quantity = 0;
-    productsCart.forEach((prod) => {
+    productsCart?.forEach((prod) => {
       quantity = quantity + prod.quantity;
     });
     return quantity;
   };
 
   const getProduct = (id) => {
-    return productsCart.find((prod) => prod.id === id);
+    return productsCart?.find((prod) => prod.id === id);
   };
 
   const getPrice = () => {
-    const totalArray = productsCart.map(
+    const totalArray = productsCart?.map(
       (itemCart) => itemCart.price * itemCart.quantity
     );
     let total = 0;
-    totalArray.forEach((itemPrice) => {
+    totalArray?.forEach((itemPrice) => {
       total = total + itemPrice;
     });
     setTotalPrice(total);
   };
 
   const isInCart = (id) => {
-    return productsCart.some((prod) => prod.id === id);
+    return productsCart?.some((prod) => prod.id === id);
   };
 
   const removeItem = (itemId) => {
-    const newList = productsCart.filter((item) => item.id !== itemId);
+    const newList = productsCart?.filter((item) => item.id !== itemId);
     setLocalStorage(newList);
   };
 
